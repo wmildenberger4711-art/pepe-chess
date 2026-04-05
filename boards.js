ae156f14d5d9c51265290058045715e59795032c
wmildenberger4711-art <wmildenberger4711@gmail.com>
2026-04-05 13:55:35 -0400

    Add files via upload

--- boards.js
+++ boards.js
@@ -4,53 +4,53 @@ return [
 
 /* 1 */
 [
-  [null,null,null,null,{type:"bishop",moves:0},null],
+  [null,{type:"queen",moves:0},null,null,null,null],
   [null,{type:"king",moves:0},null,null,null,null],
-  [null,null,{type:"queen",moves:0},null,{type:"knight",moves:0},null],
-  [{type:"rook",moves:0},{type:"pawn",moves:0},null,null,null,null],
-  [null,null,null,{type:"pawn",moves:0},null,null],
+  [null,null,{type:"knight",moves:0},null,null,null],
+  [null,{type:"pawn",moves:0},null,null,{type:"rook",moves:0},null],
+  [{type:"pawn",moves:0},null,null,{type:"knight",moves:0},null,null],
   [null,null,null,null,null,null]
-],//
+],//good
 
 /* 2 */
 [
-  [null,null,null,{type:"knight",moves:0},null,null],
-  [{type:"knight",moves:0},null,null,null,null,null],
-  [null,null,{type:"bishop",moves:0},null,null,null],
-  [null,{type:"rook",moves:0},null,null,null,null],
-  [{type:"pawn",moves:0},null,null,{type:"king",moves:0},null,null],
-  [null,null,null,null,null,null]
-],
+  [{type:"bishop",moves:0},{type:"rook",moves:0},null,{type:"rook",moves:0},null,null],
+  [null,{type:"pawn",moves:0},null,null,null,null],
+  [{type:"pawn",moves:0},null,null,null,null,{type:"bishop",moves:0}],
+  [{type:"queen",moves:0},null,null,{type:"king",moves:0},{type:"pawn",moves:0},null],
+  [null,null,null,null,null,null],
+  [null,null,null,null,null,{type:"knight",moves:0}]
+],//good
 
 /* 3 */
 [
-  [{type:"rook",moves:0},null,null,null,null,{type:"king",moves:0}],
-  [null,null,{type:"pawn",moves:0},null,null,null],
-  [null,{type:"pawn",moves:0},null,null,{type:"knight",moves:0},null],
-  [null,null,null,{type:"bishop",moves:0},null,null],
-  [{type:"queen",moves:0},null,null,null,null,null],
-  [null,null,null,null,null,null]
-],//
+  [null,{type:"queen",moves:0},null,{type:"rook",moves:0},null,null],
+  [null,{type:"pawn",moves:0},{type:"rook",moves:0},null,null,null],
+  [{type:"rook",moves:0},{type:"pawn",moves:0},null,null,null,null],
+  [{type:"pawn",moves:0},null,null,null,null,null],
+  [null,null,null,null,null,null],
+  [null,{type:"pawn",moves:0},{type:"queen",moves:0},{type:"pawn",moves:0},{type:"king",moves:0},null]
+],//good
 
 /* 4 */
 [
-  [null,null,{type:"rook",moves:0},{type:"queen",moves:0},null,null],
-  [null,null,null,null,{type:"knight",moves:0},null],
-  [null,{type:"rook",moves:0},null,{type:"pawn",moves:0},null,null],
-  [null,null,null,null,null,null],
-  [null,null,null,null,null,null],
-  [null,null,null,{type:"king",moves:0},{type:"bishop",moves:0},null]
-],//
+  [{type:"knight",moves:0},null,null,null,null,null],
+  [null,null,{type:"knight",moves:0},null,null,{type:"queen",moves:0}],
+  [null,null,{type:"king",moves:0},{type:"pawn",moves:0},null,null],
+  [null,{type:"rook",moves:0},null,{type:"knight",moves:0},null,null],
+  [null,null,{type:"rook",moves:0},null,null,null],
+  [{type:"knight",moves:0},null,null,{type:"pawn",moves:0},null,null]
+],//good
 
 /* 5 */
 [
-  [null,null,null,{type:"bishop",moves:0},null,null],
-  [{type:"queen",moves:0},null,{type:"knight",moves:0},null,null,null],
-  [{type:"rook",moves:0},null,null,{type:"king",moves:0},null,null],
-  [null,null,null,null,null,null],
-  [null,null,null,null,null,{type:"bishop",moves:0}],
-  [null,null,null,null,{type:"pawn",moves:0},null]
-],//
+  [{type:"king",moves:0},null,null,null,null,null],
+  [{type:"pawn",moves:0},{type:"rook",moves:0},null,null,{type:"knight",moves:0},null],
+  [{type:"queen",moves:0},{type:"rook",moves:0},null,null,null,{type:"bishop",moves:0}],
+  [null,null,null,null,{type:"bishop",moves:0},{type:"pawn",moves:0}],
+  [null,null,null,null,null,{type:"pawn",moves:0}],
+  [null,null,null,null,{type:"knight",moves:0},null]
+],//good
 
 /* 6 */
 [
@@ -60,45 +60,46 @@ return [
   [null,null,{type:"knight",moves:0},null,null,null],
   [{type:"knight",moves:0},null,null,{type:"knight",moves:0},null,null],
   [{type:"king",moves:0},{type:"knight",moves:0},null,null,null,{type:"knight",moves:0}]
-],
+],//good
 
 /* 7 */
 [
-  [null,{type:"queen",moves:0},null,{type:"queen",moves:0},null,null],
-  [null,null,null,null,null,null],
-  [{type:"knight",moves:0},null,{type:"king",moves:0},null,null,{type:"rook",moves:0}],
-  [{type:"bishop",moves:0},null,null,{type:"pawn",moves:0},null,{type:"rook",moves:0}],
-  [null,{type:"knight",moves:0},{type:"pawn",moves:0},null,{type:"pawn",moves:0},null],
-  [null,null,null,null,null,null]
-],//
+  [{type:"rook",moves:0},null,null,null,null,{type:"king",moves:0}],
+  [null,null,null,null,{type:"pawn",moves:0},null],
+  [null,null,null,null,null,{type:"pawn",moves:0}],
+  [null,{type:"knight",moves:0},null,null,null,null],
+  [null,{type:"bishop",moves:0},null,null,null,null],
+  [{type:"pawn",moves:0},null,null,null,null,{type:"queen",moves:0}]
+],//good
 
 /* 8 */
 [
   [{type:"king",moves:0},null,null,null,null,null],
   [null,{type:"pawn",moves:0},null,null,null,null],
-  [null,null,null,null,null,null],
+  [{type:"pawn",moves:0},null,null,null,null,null],
   [null,null,null,null,null,null],
   [null,null,null,null,null,null],
   [null,null,null,null,null,null]
-],//
+],//good
+
 /* 9 */
 [
-  [null,{type:"rook",moves:0},{type:"queen",moves:0},null,{type:"knight",moves:0},null],
+  [null,{type:"king",moves:0},null,{type:"bishop",moves:0},null,null],
+  [null,{type:"pawn",moves:0},{type:"pawn",moves:0},null,null,null],
+  [{type:"pawn",moves:0},null,{type:"knight",moves:0},null,null,null],
   [null,null,null,null,null,null],
-  [{type:"bishop",moves:0},null,null,{type:"pawn",moves:0},null,null],
-  [null,null,null,{type:"king",moves:0},null,null],
-  [null,null,{type:"knight",moves:0},null,null,null],
-  [null,null,null,null,null,null]
-],//
+  [null,null,null,null,null,null],
+  [null,null,{type:"queen",moves:0},null,null,{type:"bishop",moves:0}]
+],//g
 
 /* 10 */
 [
-  [null,null,{type:"queen",moves:0},null,{type:"queen",moves:0},null],
-  [null,null,null,null,{type:"queen",moves:0},null],
+  [null,null,null,null,null,{type:"pawn",moves:0}],
+  [null,null,null,{type:"queen",moves:0},{type:"bishop",moves:0},null],
+  [null,{type:"knight",moves:0},null,null,null,{type:"rook",moves:0}],
   [null,null,null,null,null,null],
-  [null,null,{type:"king",moves:0},null,{type:"queen",moves:0},{type:"queen",moves:0}],
-  [null,null,null,null,null,null],
-  [null,null,{type:"queen",moves:0},null,{type:"queen",moves:0},null]
+  [null,null,null,null,{type:"king",moves:0},null],
+  [{type:"pawn",moves:0},null,null,null,{type:"rook",moves:0},{type:"rook",moves:0}]
 ]
 
 ];
